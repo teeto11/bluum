41954b8633aee05350e262866670cdf36979496b
Dafomchollom <ayomidechollom@gmail.com>
2019-02-22 15:31:06 +0100

    dafom2

--- resources/views/expert/index.blade.php
+++ resources/views/expert/index.blade.php
@@ -27,7 +27,7 @@
                             <section class="expert-summary">
                                 <p>{{ ucfirst($details->about) }}.</p>
                             </section>
-                            <section class="expert-footer" style="padding: 1rem" >
+                            <section class="expert-footer" style="" >
                                 <span class="exp"><b>WORK EXPERIENCE : </b>{{ $details->experience }} YEARS</span>
                                 <div class="small">
                                     <section>
