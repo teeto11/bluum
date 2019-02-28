71ec32e79228999cda26c0f04f0d765d8e61ce32
Dafomchollom <ayomidechollom@gmail.com>
2019-02-28 22:47:51 +0100

    dafom adds changes

--- resources/views/widgets/footer.blade.php
+++ resources/views/widgets/footer.blade.php
@@ -3,26 +3,49 @@
     <div class="footer__head">
         <div class="container">
             <div class="row">
-                <div class="footer__links col-sm-12 col-xs-6 col-md-2">
+                <div class="footer__links col-sm-6 col-xs-6 col-md-2">
                     <ul>
                         <li class="link__head">Company</li>
                         <li><a href="#"><i class="fa fa-angle-double-right"></i> About Us</a></li>
                         <li><a href="#"><i class="fa fa-angle-double-right"></i> Contact Us</a></li>
                         <li><a href="#"><i class="fa fa-angle-double-right"></i> Careers</a></li>
-                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Terms &amp; Conditions</a></li>
-                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Privacy Policy</a></li>
                     </ul>
                 </div>
-                <div class="footer__links col-sm-12 col-xs-6 col-md-2">
+                <div class="footer__links col-sm-6 col-xs-6 col-md-2">
                     <ul>
                         <li class="link__head">Learn More</li>
+                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Terms &amp; Conditions</a></li>
+                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Privacy Policy</a></li>
                         <li><a href="#"><i class="fa fa-angle-double-right"></i> FAQ</a></li>
                         <li><a href="#"><i class="fa fa-angle-double-right"></i> Q &amp; A</a></li>
                         <li><a href="#"><i class="fa fa-angle-double-right"></i> Learn</a></li>
                         <li><a href="#"><i class="fa fa-angle-double-right"></i> Ask an Expert</a></li>
-                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Events</a></li>
-                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Site Guidelines</a></li>
                     </ul>
+                </div>
+                <div class="footer__links col-sm-6 col-xs-6 col-md-2">
+                    <ul>
+                        <li class="link__head">Categories</li>
+                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Random</a></li>
+                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Education</a></li>
+                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Science</a></li>
+                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Politics</a></li>
+                    </ul>
+                </div>
+                <div class="footer__links col-sm-6 col-xs-6 col-md-2">
+                    <ul>
+                        <li class="link__head">Links</li>
+                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Blog</a></li>
+                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Ask</a></li>
+                        <li><a href="#"><i class="fa fa-angle-double-right"></i> Experts</a></li>
+                    </ul>
+                </div>
+                <div class="footer__links col-sm-12 col-xs-12 col-md-4">
+                    <div class="footer-social-bar">
+                        <a href=""><i class="fa fa-facebook"></i></a>
+                        <a href=""><i class="fa fa-twitter"></i></a>
+                        <a href=""><i class="fa fa-linkedin"></i></a>
+                        <a href=""><i class="fa fa-instagram"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
