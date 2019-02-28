1a55ae939d40fdae38b30153f879c41165e975b3
festus <festusyuma@gmail.com>
2019-02-28 21:29:30 +0100

    festus

--- resources/views/question/ask.blade.php
+++ resources/views/question/ask.blade.php
@@ -74,146 +74,40 @@
                             <div class="posts__activity">Activity</div>
                         </div>
                         <div class="posts__body">
-                            <div class="posts__item">
-                                <div class="posts__section-left">
-                                    <div class="posts__topic">
-                                        <div class="posts__content">
-                                            <a href="#">
-                                                <h3>Current news and discussion</h3>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div class="posts__category"><a href="#" class="category"><i class="bg-368f8b"></i>Politics</a></div>
-                                </div>
-                                <div class="posts__section-right">
-                                    <div class="posts__users">
-                                        <div>
-                                            <a href="#" class="avatar"><img src="./assets/fonts/icons/avatars/A.svg" alt="avatar"></a>
-                                        </div>
-                                        <div>
-                                            <a href="#" class="avatar"><img src="./assets/fonts/icons/avatars/G.svg" alt="avatar"></a>
-                                        </div>
-                                        <div>
-                                            <a href="#" class="avatar"><img src="./assets/fonts/icons/avatars/P.svg" alt="avatar"></a>
-                                        </div>
-                                    </div>
-                                    <div class="posts__replies">31</div>
-                                    <div class="posts__views">14.5k</div>
-                                    <div class="posts__activity">13d</div>
-                                </div>
-                            </div>
-                            <div class="posts__item bg-f2f4f6">
-                                <div class="posts__section-left">
-                                    <div class="posts__topic">
-                                        <div class="posts__content">
-                                            <a href="#">
-                                                <h3>Get your username drawn by the other users of unity! or a drawing based on what you do</h3>
-                                            </a>
-                                            <div class="posts__tags tags">
-                                                <a href="#" class="bg-4f80b0">gaming</a>
-                                                <a href="#" class="bg-424ee8">nature</a>
-                                                <a href="#" class="bg-36b7d7">entertainment</a>
+                            @php $counter = 1; @endphp
+                            @foreach ($popular_questions as $question)
+                                <div class="posts__item {{ ($counter%2 == 0) ? 'bg-f2f4f6' : '' }}">
+                                    <div class="posts__section-left">
+                                        <div class="posts__topic">
+                                            <div class="posts__content">
+                                                <a href="/question/{{ $question->id }}/{{ formatUrlString($question->title) }}">
+                                                    <h3>{{ $question->title }}</h3>
+                                                </a>
+                                                <div class="posts__tags tags">
+                                                    @php $tags = explode(',', $question->tags) @endphp
+                                                    @foreach($tags as $tag)
+                                                        <a href="#" class="bg-4f80b0">{{ $tag }}</a>
+                                                    @endforeach
+                                                </div>
                                             </div>
                                         </div>
+                                        <div class="posts__category"><a href="/questions/category/{{ $question->category }}" class="category"><i class="bg-368f8b"></i>{{ ucfirst($question->category) }}</a></div>
                                     </div>
-                                    <div class="posts__category"><a href="#" class="category"><i class="bg-4436f8"></i>Video</a></div>
-                                </div>
-                                <div class="posts__section-right">
-                                    <div class="posts__users">
-                                        <div>
-                                            <a href="#" class="avatar"><img src="./assets/fonts/icons/avatars/L.svg" alt="avatar"></a>
+                                    <div class="posts__section-right">
+                                        <div class="posts__users">
+                                            @foreach($question->popularReplies as $reply)
+                                                <div>
+                                                    <a href="#" class="avatar"><img src="{{ asset('fonts/icons/avatars/'.getFirstLetterUppercase($reply->user->firstname)).'.svg' }}" alt="avatar"></a>
+                                                </div>
+                                            @endforeach
                                         </div>
-                                        <div>
-                                            <a href="#" class="avatar"><img src="./assets/fonts/icons/avatars/T.svg" alt="avatar"></a>
-                                        </div>
+                                        <div class="posts__replies">{{ count($question->replies) }}</div>
+                                        <div class="posts__views">{{ $question->views }}</div>
+                                        <div class="posts__activity">{{ getLastActivityTime($question->updated_at) }}</div>
                                     </div>
-                                    <div class="posts__replies">252</div>
-                                    <div class="posts__views">396</div>
-                                    <div class="posts__activity">13m</div>
                                 </div>
-                            </div>
-                            <div class="posts__item">
-                                <div class="posts__section-left">
-                                    <div class="posts__topic">
-                                        <div class="posts__content">
-                                            <a href="#">
-                                                <h3>Which movie have you watched most recently?</h3>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div class="posts__category"><a href="#" class="category"><i class="bg-3ebafa"></i> Exchange</a></div>
-                                </div>
-                                <div class="posts__section-right">
-                                    <div class="posts__users">
-                                        <div>
-                                            <a href="#" class="avatar"><img src="./assets/fonts/icons/avatars/E.svg" alt="avatar"></a>
-                                        </div>
-                                        <div>
-                                            <a href="#" class="avatar"><img src="./assets/fonts/icons/avatars/I.svg" alt="avatar"></a>
-                                        </div>
-                                        <div>
-                                            <a href="#" class="avatar"><img src="./assets/fonts/icons/avatars/R.svg" alt="avatar"></a>
-                                        </div>
-                                    </div>
-                                    <div class="posts__replies">207</div>
-                                    <div class="posts__views">7.5k</div>
-                                    <div class="posts__activity">41m</div>
-                                </div>
-                            </div>
-                            <div class="posts__item posts__item--bg-gradient">
-                                <div class="posts__section-left">
-                                    <div class="posts__topic">
-                                        <div class="posts__content">
-                                            <a href="#">
-                                                <h3><span>This post contails spoiler about</span> Star Wars Movie.</h3>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div class="posts__category"><a href="#" class="category"><i class="bg-777da7"></i> Q&amp;As</a></div>
-                                </div>
-                                <div class="posts__section-right">
-                                    <div class="posts__users">
-                                        <div>
-                                            <a href="#" class="avatar"><img src="./assets/fonts/icons/avatars/F.svg" alt="avatar"></a>
-                                        </div>
-                                    </div>
-                                    <div class="posts__replies">2.3k</div>
-                                    <div class="posts__views">2.0k</div>
-                                    <div class="posts__activity">1h</div>
-                                </div>
-                            </div>
-                            <div class="posts__item">
-                                <div class="posts__section-left">
-                                    <div class="posts__topic">
-                                        <div class="posts__content">
-                                            <a href="#">
-                                                <h3>Take a picture of what you’re doing at this very moment</h3>
-                                            </a>
-                                            <div class="posts__tags tags">
-                                                <a href="#" class="bg-ec008c">selfie</a>
-                                                <a href="#" class="bg-7cc576">camera</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="posts__category"><a href="#" class="category"><i class="bg-c6b38e"></i> Pets</a></div>
-                                </div>
-                                <div class="posts__section-right">
-                                    <div class="posts__users">
-                                        <div>
-                                            <a href="#" class="avatar"><img src="./assets/fonts/icons/avatars/C.svg" alt="avatar"></a>
-                                        </div>
-                                        <div>
-                                            <a href="#" class="avatar"><img src="./assets/fonts/icons/avatars/U.svg" alt="avatar"></a>
-                                        </div>
-                                        <div>
-                                            <a href="#" class="avatar"><img src="./assets/fonts/icons/avatars/I.svg" alt="avatar"></a>
-                                        </div>
-                                    </div>
-                                    <div class="posts__replies">441</div>
-                                    <div class="posts__views">3.1k</div>
-                                    <div class="posts__activity">6h</div>
-                                </div>
-                            </div>
+                                @php $counter++ @endphp
+                            @endforeach
                         </div>
                     </div>
                 </div>
