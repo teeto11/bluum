a918d2334885bd62184204b30c51f6e5dc5ea390
Dafomchollom <ayomidechollom@gmail.com>
2019-03-06 12:00:15 +0100

    changes dafom

--- resources/views/user/answers.blade.php
+++ resources/views/user/answers.blade.php
@@ -4,10 +4,41 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/hover.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/animate.css') }}" />
+    <style>
+        @media (max-width:1040px){
+            .back-btn{
+                position:absolute;
+                top: 4%;
+            }
+        }
+        @media (max-width:568px){
+            .back-btn{
+                position:absolute;
+                top: 4%;
+            }
+        }
+        @media (max-width:532px){
+            .back-btn{
+                position:absolute;
+                top: 5%;
+                left:5%;
+            }
+            .questions{
+                padding:0px !important;
+                box-shadow:2px 2px 5px rgb(165, 165, 165) !important;
+            }
+            td{
+                padding:15px 5px !important;
+            }
+        }
+    </style>
 @endsection
 
 @section('profile-main')
 <section class="questions">
+<div style="padding-left: 2rem" >
+            <a href="" class="back-btn" ><i class="fa fa-arrow-left" ></i></a>
+        </div>
     <div class="table-row">
         <div class="table-responsive bg-white">
             <h3 class="">User Responses</h3><hr>
