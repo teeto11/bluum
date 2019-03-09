42263f1304e2a3f00476b1e5c162b6df34afa5ab
Dafomchollom <ayomidechollom@gmail.com>
2019-03-09 16:48:05 +0100

    new change

--- resources/views/widgets/top-nav-bar.blade.php
+++ resources/views/widgets/top-nav-bar.blade.php
@@ -32,10 +32,10 @@
                 </div>
                 <nav class="dropdown dropdown--design-01" data-dropdown-list="menu">
                     <div>
-                        <ul class="dropdown__catalog row">
+                        <ul class="dropdown__catalog row md-none">
                             <li class="col-xs-6"><a href="{{ route('login') }}">Sign In</a></li>
                             <li class="col-xs-6"><a href="{{ route('register') }}">Sign Up</a></li>
-                        </ul><hr>
+                        </ul><hr class="divider-hr">
                         <ul class="dropdown__catalog row">
                             <li class="col-xs-6"><a href="{{ route('question.create') }}">Ask</a></li>
                             <li class="col-xs-6"><a href="{{ route('experts') }}">Experts</a></li>
@@ -52,7 +52,7 @@
                             <li class="col-xs-6"><a href="{{ route('question.showbycategory', formatUrlString('common illness')) }}" class="category"><i class="bg-777da7"></i>Common illness</a></li>
                             <li class="col-xs-6"><a href="{{ route('question.showbycategory', formatUrlString('special illness')) }}" class="category"><i class="bg-368f8b"></i>Special illness</a></li>
                         </ul>
-                        <hr>
+                        <hr class="divider-hr">
                         <ul class="dropdown__catalog row md-none">
                             <li class="col-xs-6"><a href="{{ route('admin.home') }}"> Dashboard</a></li>
                             <li class="col-xs-6"><a href="{{ route('user.profile') }}">Profile</a></li>
