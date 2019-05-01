45fb8136689028e297d8b50d859dfac2c391cfa0
festus <festusyuma@gmail.com>
2019-05-01 09:09:23 +0100

    festus

---
+++ app/Notifications/CorrectAnswerNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class CorrectAnswerNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                ->priority(1)
+                ->from('info@bluumhealth.com', 'Bluumhealth')
+                ->subject('Correct Answer')
+                ->greeting('Hello')
+                ->line('Your answer to has been marked as a correct answer')
+                ->action('Question', '')
+                ->line('We look forward to seeing you try out more questions');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
